84a6074c5448243bec4b282114543251e4ae7763
spacewei <693906369@qq.com>
2017-02-13 21:31:43 +0800

    Revert "Revert "20170213c""
    This reverts commit 4114d5f3c6971a35a3c30e81f92be5a03c623843.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by SPACEY on 2017/2/12.
+ */
+//document.getElementById('app').innerHTML="这是我第三个打包成功的程序";
+import CV from "./components/CV.vue"
+
+let app = new Vue({
+    el: '#app'
+});
